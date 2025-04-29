3e9e6dfc503a92fcc622d51f6b9c6954fd391411
David Cooper <dcooper@wcupa.edu>
2025-04-29 15:17:19 -0400

    Stream to stop memory leak

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -7,4 +7,14 @@ model = YOLO("yolov8n.pt")
 #results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model(0, show = True)
+results = model(0, stream=True)  # return a generator of Results objects
+
+# Process results generator
+for result in results:
+    #boxes = result.boxes  # Boxes object for bounding box outputs
+    #masks = result.masks  # Masks object for segmentation masks outputs
+    #keypoints = result.keypoints  # Keypoints object for pose outputs
+    #probs = result.probs  # Probs object for classification outputs
+    %obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    #result.save(filename="result.jpg")  # save to disk
